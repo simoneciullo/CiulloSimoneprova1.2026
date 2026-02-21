56e1f609de1a8614bc1a2516bad21dbd1d467dd0
4a <4a@labinf1-05>
2026-02-21 11:00:32 +0100

    versione 1

---
+++ src/Libreria.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Libreria {
+	private String nome;
+	//private int maxLibri;
+	private Libro[] libri;
+	//private Libro libri[];
+	private int numLibri;
+	
+	public Libreria(String nome, int maxLibri) {
+		libri=new Libro[maxLibri];
+		numLibri=0;
+		this.nome=nome;
+	}
+	/*public boolean addLibro(Libro lib) {
+		libri[numLibri]=lib;
+		numLibri++;
+		return false;
+	}*/
+	public boolean addLibro(Libro lib) {
+		if(numLibri<libri.length) {
+			libri[numLibri]=lib;
+			numLibri++;
+			return true;
+		}
+		return false;
+	}
+	/*@Override
+	public String toString() {
+		return "Libreria [nome=" + nome + ", libri=" + Arrays.toString(libri) + "]";
+	*/
+	//elimina libro per posizione senza controllo
+	
+	public void delLibroPos (int pos) {
+		libri[pos]=null;
+	}
+	
+	//elimina libro per codice con controllo
+	
+	public boolean delLibroCodice (String cod) {
+		for(int i=0; i<libri.length; i++) {
+			if(libri[i]!=null && libri[i].getCodice().equalsIgnoreCase(cod)) {
+				libri[i]=null;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String toString() {
+		String s="Nome Libreria: "+nome+"\n";
+		for(int i=0; i<libri.length; i++) {
+			if(libri[i]!=null)
+				s+=libri[i]+"\n";
+		}
+		
+		return s;
+	}
+	public static void main(String[] args) {
+		Libreria libCentrale=new Libreria("libreria viale ferrari", 3);
+		Libro lib1=new Libro("libro 1", "autore 1");
+		Libro lib2=new Libro("libro 2", "autore 2");
+		System.out.println(libCentrale.addLibro(lib1));
+		System.out.println(libCentrale.addLibro(lib2));
+		System.out.println(libCentrale); 
+	}
+}
---
+++ src/Libro.java
@@ -0,0 +1,31 @@
+
+public class Libro {
+	private String titolo;
+	private String autore;
+	private String codice;
+	private int pagine;
+	
+	public Libro(String titolo, String autore) {
+		this.titolo=titolo;
+		this.autore=autore;
+	}
+	public void setCodice(String codice) {
+		this.codice=codice;
+	}
+	
+	public String getCodice() {
+		return codice;
+	}
+	public void setPagine(int pagine) {
+		this.pagine=pagine;
+	}
+	public String toString() {
+		return "Libro \ntitolo:"+titolo+"\nautore: "+autore+"\ncodice: "+codice+"\npagine: "+pagine;
+	}
+	public static void main(String[] args) {
+		Libro l1=new Libro("il piccolo principe", "simone ciullo");
+		l1.setCodice("cod1234");
+		l1.setPagine(1438);
+		System.out.println(l1);
+	}
+}
